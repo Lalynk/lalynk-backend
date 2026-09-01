12937c266f8a61d5d909b1267a28153775577160
sucramdev <marcusvonschantz@gmail.com>
2026-09-01 14:47:41 +0200

    chore: initialize backend setup

---
+++ src/main/java/com/lalynk/lalynk_backend/LalynkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lalynk.lalynk_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalynkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LalynkBackendApplication.class, args);
+	}
+
+}
