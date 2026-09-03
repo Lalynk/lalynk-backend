4ffc7b430cfc196ccdce53dec66935e04529d4f6
sucramdev <marcusvonschantz@gmail.com>
2026-09-03 19:59:08 +0200

    feat: add persmissions for authentication

--- src/main/java/com/lalynk/lalynk_backend/users/UserController.java
+++ src/main/java/com/lalynk/lalynk_backend/users/UserController.java
@@ -1,6 +1,7 @@
 package com.lalynk.lalynk_backend.users;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +22,7 @@ public class UserController {
         return new ResponseEntity<>(userService.createUser(createUserRequest), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAuthority('users:read')")
     @GetMapping("")
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.ACCEPTED);
