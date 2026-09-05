2b3fed01c2d66557ade495a23dd7de7c6fd148d3
sucramdev <marcusvonschantz@gmail.com>
2026-09-05 22:46:30 +0200

    test: add unit tests for service layer

--- src/main/java/com/lalynk/lalynk_backend/config/SecurityConfig.java
+++ src/main/java/com/lalynk/lalynk_backend/config/SecurityConfig.java
@@ -15,9 +15,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-
-    http.authorizeHttpRequests(auth ->
-            auth.anyRequest().authenticated())
+    http.authorizeHttpRequests(authorize ->
+            authorize.requestMatchers("/secrets/public/**").permitAll().anyRequest().authenticated())
             .oauth2ResourceServer(oauth2 ->
                     oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
