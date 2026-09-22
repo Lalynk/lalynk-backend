eaafd3ca1bfa2f5d7a260e8450fc06dc86982a3e
sucramdev <marcusvonschantz@gmail.com>
2026-09-22 15:43:38 +0200

    feat: force Auth0 re-authentication on login

--- src/main/java/com/lalynk/lalynk_backend/config/SecurityConfig.java
+++ src/main/java/com/lalynk/lalynk_backend/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.lalynk.lalynk_backend.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +7,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -19,7 +21,7 @@ public class SecurityConfig {
     private String frontendUrl;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, OAuth2AuthorizationRequestResolver authorizationRequestResolver) throws Exception{
     http.cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
             .authorizeHttpRequests(authorize -> authorize.requestMatchers
@@ -30,13 +32,29 @@ public class SecurityConfig {
                     .anyRequest().authenticated())
 
             .oauth2Login(oauth2 ->
-                    oauth2.defaultSuccessUrl(frontendUrl, true));
+                    oauth2.defaultSuccessUrl(frontendUrl, true).authorizationEndpoint(endpoint ->
+                            endpoint.authorizationRequestResolver(authorizationRequestResolver))
+
+            );
         return http.build();
     }
 
     @Bean
-        public GrantedAuthoritiesMapper useAuthoritiesMapper() {
+    public GrantedAuthoritiesMapper useAuthoritiesMapper() {
             return new AuthoritiesMapper();
+    }
+
+    @Bean
+    public OAuth2AuthorizationRequestResolver authorizationRequestResolver(ClientRegistrationRepository clientRegistrationRepository) {
+
+        DefaultOAuth2AuthorizationRequestResolver resolver = new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
+
+        resolver.setAuthorizationRequestCustomizer(customizer ->
+                customizer.additionalParameters(params ->
+                        params.put("prompt", "login")
+                )
+        );
+        return resolver;
     }
 
 }
